140848c118dd14bff547bc36eafcc14a0f27d4d1
alexgleon <alexgleon@hotmail.com>
2017-04-24 22:16:18 -0600

    Add files via upload

--- plot4.R
+++ plot4.R
@@ -11,13 +11,14 @@ source <- read.table(File, header=TRUE, sep=";", stringsAsFactors=FALSE, dec="."
 subset <- source[source$Date %in% c("1/2/2007","2/2/2007") ,]
 
 # Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
-sdatetime <- strptime(paste(subset$Date, subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+datetime <- strptime(paste(subset$Date, subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subset$Global_active_power)
 globalReactivePower <- as.numeric(subset$Global_reactive_power)
 voltage <- as.numeric(subset$Voltage)
 subMetering1 <- as.numeric(subset$Sub_metering_1)
 subMetering2 <- as.numeric(subset$Sub_metering_2)
 subMetering3 <- as.numeric(subset$Sub_metering_3)
+
 
 par(mfrow = c(2, 2)) 
 
@@ -28,10 +29,8 @@ plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
 plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, subMetering2, type="l", col="red")
 lines(datetime, subMetering3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
-
-## Saving to file
-dev.copy(png, file="plot3.png", height=480, width=480)
+dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
