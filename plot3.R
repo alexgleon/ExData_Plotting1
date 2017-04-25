05cedabc0dc30f6980d6e9c8aeb93db8ee3c3dcf
alexgleon <alexgleon@hotmail.com>
2017-04-24 21:51:21 -0600

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -7,26 +7,20 @@ setwd("~/RWkngdrctry/plotting")
 File <- "household_power_consumption.txt"
 
 # Reading, naming and subsetting data
-source <- read.table(File,skip=1,sep=";")
-names(source) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+source <- read.table(File, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subset <- source[source$Date %in% c("1/2/2007","2/2/2007") ,]
 
 # Transforming the Date and Time vars from characters into objects of type Date and POSIXlt respectively
-subset$Date <- as.Date(subset$Date, format="%d/%m/%Y")
-subset$Time <- strptime(subset$Time, format="%H:%M:%S")
-subset[1:1440,"Time"] <- format(subset[1:1440,"Time"],"2007-02-01 %H:%M:%S")
-subset[1441:2880,"Time"] <- format(subset[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+datetime <- strptime(paste(subset$Date, subset$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subset$Global_active_power)
+subMetering1 <- as.numeric(subset$Sub_metering_1)
+subMetering2 <- as.numeric(subset$Sub_metering_2)
+subMetering3 <- as.numeric(subset$Sub_metering_3)
 
-
-#plotting data
-plot(subset$Time,subset$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
-with(subset,lines(Time,as.numeric(as.character(Sub_metering_1))))
-with(subset,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
-with(subset,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
-legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-
-# annotating graph
-title(main="Energy sub-metering")
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
 
 ## Saving to file
 dev.copy(png, file="plot3.png", height=480, width=480)
